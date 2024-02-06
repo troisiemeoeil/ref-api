5c9512ec2987e8e8662b2fdc73f6c8b4864ebae3
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2024-02-06 23:53:58 +0100

    first commit

---
+++ components/SmoothScroll.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+import {ReactLenis} from "@studio-freight/react-lenis"
+
+function SmoothScroll({children}) {
+    return <ReactLenis root>
+        {children}
+        </ReactLenis>
+}
+
+export default SmoothScroll
